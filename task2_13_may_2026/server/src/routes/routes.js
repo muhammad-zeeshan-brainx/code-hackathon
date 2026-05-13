e0da48aafc735e93a86eea83694c8cb3999794cc
muhammad-zeeshan-brainx <muhammad.zeeshan@brainxtech.com>
2026-05-13 13:06:39 +0500

    base tmplate added

---
+++ task2_13_may_2026/server/src/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/helth", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Server is healthy",
+  });
+});
+
+module.exports = router;
